4e4cc356731fff717cb65eff12e39f8fc71e9ca5
Ruben Barros <ruben.23@live.com.pt>
2014-11-10 17:24:37 +0000

    some models

---
+++ app/models/Group.scala
@@ -0,0 +1,11 @@
+package models
+
+/**
+ * Created by ruben on 10-11-2014.
+ *
+ */
+case class Group(name: String, password: String, users: List[String])
+
+object Group {
+
+}
---
+++ app/models/User.scala
@@ -0,0 +1,12 @@
+package models
+
+/**
+ * Created by ruben on 10-11-2014.
+ *
+ */
+
+case class User (id: Int, username: String, first_name: String, last_name: String, password: String, lat: String, lon: String, timestamp: Long) //groups: List[String/url]
+
+object User {
+
+}
