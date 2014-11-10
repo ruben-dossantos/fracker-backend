61a49916e52ea89aad67a8c685b96fb9ae6143ee
Ruben Barros <ruben.23@live.com.pt>
2014-11-10 17:24:45 +0000

    trying out play

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import argonaut.Argonaut._
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def getUsers = Action {
+    Ok("{'name': 'ruben', password: '30vinte'}").as("application/json")
   }
 
 }
